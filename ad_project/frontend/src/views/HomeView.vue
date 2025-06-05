c14c497e5179b02797f46700102b1e2e9f1ddc21
Alim4ik322 <assanovali23@gmail.com>
2025-06-05 13:17:36 +0300

    пункт 5.5

--- ad_project/frontend/src/views/HomeView.vue
+++ ad_project/frontend/src/views/HomeView.vue
@@ -1,47 +1,60 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" xs="12">
-       <v-carousel>
-        <v-carousel-item
-          v-for="ad in ads.filter(ad => ad.promo)"
+  <div>
+    <!-- Карусель с фильтрацией promo -->
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12">
+          <v-carousel>
+            <v-carousel-item
+              v-for="ad in ads.filter(ad => ad.promo)"
+              :key="ad.id"
+              :src="ad.src"
+              cover
+            >
+              <div class="ad-link">
+                <v-btn class="error" :to="'/ad/' + ad.id">
+                  {{ ad.title }}
+                </v-btn>
+              </div>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- Сетка карточек -->
+    <v-container>
+      <v-row justify="center" align="stretch">
+        <v-col
+          v-for="ad in ads"
           :key="ad.id"
-          :src="ad.src"
-          cover
-  >
-    <div class="ad-link">
-      <v-btn class="error" :to="'/ad/' + ad.id">
-        {{ ad.title }}
-      </v-btn>
-    </div>
-  </v-carousel-item>
-</v-carousel>
-
-      </v-col>
-    </v-row>
-  </v-container>
-
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" xs="12">
-        <v-card>
-          <v-img height="200px"></v-img>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb0">Здесь будет заголовок</h3>
-              <div>Здесь будет описание</div>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text>Open</v-btn>
-            <v-btn raised color="primary">Buy</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-card>
+            <v-img
+              :src="ad.src"
+              height="200px"
+            ></v-img>
+            <v-card-title>
+              <div>
+                <h3 class="headline mb-0">{{ ad.title }}</h3>
+                <div>{{ ad.desc }}</div>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+              <v-btn color="primary">Buy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
 
 
 <script>
