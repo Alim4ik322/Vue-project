fee939bc8bd36329f9b9d69d8ed807cad6aaeb38
Alim4ik322 <assanovali23@gmail.com>
2025-06-14 11:04:59 +0300

    пункт 11.3

--- ad_project/frontend/src/views/Ads/NewAdView.vue
+++ ad_project/frontend/src/views/Ads/NewAdView.vue
@@ -4,44 +4,60 @@
       <v-col cols="8" offset="2">
         <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
 
-        <!-- Форма -->
-        <v-form v-model="valid" ref="form">
+        <v-form v-model="valid" ref="form" validation>
           <v-text-field
+            name="title"
             label="Ad Title"
+            type="text"
             v-model="title"
-            :rules="[(v) => !!v || 'Title is required']"
+            :rules="[v => !!v || 'Title is required']"
           ></v-text-field>
 
           <v-textarea
+            name="description"
             label="Ad Description"
+            type="text"
             v-model="description"
-            :rules="[(v) => !!v || 'Description is required']"
+            :rules="[v => !!v || 'Description is required']"
             class="mb-3"
           ></v-textarea>
+
+          <v-layout row>
+            <v-flex xs12>
+              <v-btn class="mt-3" color="warning">
+                Upload
+                <v-icon right dark>mdi-cloud-upload</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12>
+              <img
+                src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+                height="150"
+                class="mt-3"
+              />
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12>
+              <v-switch
+                v-model="promo"
+                label="Ad to Promo?"
+              ></v-switch>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12>
+              <v-spacer></v-spacer>
+              <v-btn color="success" @click="createAd">Create Ad</v-btn>
+            </v-flex>
+          </v-layout>
         </v-form>
 
-        <!-- Кнопка загрузки -->
-        <v-btn class="mt-3" color="warning" @click="uploadImage">
-          Upload
-          <v-icon right dark>mdi-cloud-upload</v-icon>
-        </v-btn>
-
-        <!-- Превью картинки -->
-        <img :src="src" height="150" class="mt-3" />
-
-        <!-- Переключатель -->
-        <v-switch v-model="promo" label="Ad to Promo?"></v-switch>
-
-        <!-- Кнопка создания -->
-        <v-btn
-          color="success"
-          class="mt-3"
-          @click="createAd"
-          :loading="loading"
-          :disabled="!valid || loading"
-        >
-          Create Ad
-        </v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -52,28 +68,23 @@ export default {
   data() {
     return {
       valid: false,
-      title: '',
-      description: '',
-      promo: false,
-      src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-      loading: false
-    }
+      title: "",
+      description: "",
+      promo: false
+    };
   },
   methods: {
-    uploadImage() {
-      console.log('Upload clicked')
-    },
-    createAd(){
-      if (this.$refs.form.validate()){
+    createAd() {
+      if (this.$refs.form.validate()) {
         const ad = {
           title: this.title,
           desc: this.description,
           promo: this.promo,
           src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
-        }
-        console.log(ad)
+        };
+        this.$store.dispatch("createAd", ad);
       }
     }
   }
-}
+};
 </script>
