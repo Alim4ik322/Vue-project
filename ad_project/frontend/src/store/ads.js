b528423cd2c77ae8cc82da03cf74a878106ecec9
Alim4ik322 <assanovali23@gmail.com>
2025-06-19 13:06:58 +0300

    пункт 16.1

--- ad_project/frontend/src/store/ads.js
+++ ad_project/frontend/src/store/ads.js
@@ -1,35 +1,39 @@
 export default {
 state: {
-    ads:[
-			{
-				title:"First",
-				desc:"First Desc",
-				promo: true,
-		src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-				id:"1"
-			},
-			{
-				title:"Second",
-				desc:"Second Desc",
-				promo: true,
-			src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-				id:"2"
-			},
-			{
-				title:"Third",
-				desc:"Thitd Desc",
-				promo: true,
-			src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-				id:"3"
-			},
-			{
-				title:"Fouth",
-				desc:"Fouth Desc",
-				promo: true,
-			src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-				id:"4"
-			}
-	]
+  ads: [
+      {
+        title: 'First',
+        desc: 'First Desc',
+        promo: true,
+        src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        id: '1',
+        userId: '1' // Добавлено
+      },
+      {
+        title: 'Second',
+        desc: 'Second Desc',
+        promo: true,
+        src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        id: '2',
+        userId: '2' // Добавлено
+      },
+      {
+        title: 'Third',
+        desc: 'Third Desc', // Исправлено Thitd -> Third
+        promo: true,
+        src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        id: '3',
+        userId: '1' // Добавлено
+      },
+      {
+        title: 'Fourth', // Исправлено Fouth -> Fourth
+        desc: 'Fourth Desc', // Исправлено Fouth -> Fourth
+        promo: true,
+        src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        id: '4',
+        userId: '2' // Добавлено
+      }
+    ]
 
 },
 mutations: {
