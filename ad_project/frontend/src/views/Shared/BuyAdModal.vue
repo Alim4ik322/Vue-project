7f3e16ad2b492ca83383b85bb1e377e148bafb94
Alim4ik322 <assanovali23@gmail.com>
2025-06-19 21:58:56 +0300

    пункт 18.3

--- ad_project/frontend/src/views/Shared/BuyAdModal.vue
+++ ad_project/frontend/src/views/Shared/BuyAdModal.vue
@@ -42,8 +42,8 @@ v-model="phone"
 <v-col cols="12">
 <v-card-actions>
 <v-spacer></v-spacer>
-<v-btn>Close</v-btn>
-<v-btn color="success">Buy It!</v-btn>
+<v-btn @click="onClose">Close</v-btn>
+<v-btn @click="onSave" color="success">Buy It!</v-btn>
 </v-card-actions>
 </v-col>
 </v-row>
@@ -60,6 +60,28 @@ export default {
             name: '',
             phone: ''
         }
+    },
+    methods: {
+        onClose (){
+            this.name = ""
+            this.phone = ""
+            this.modal = false
+        },
+        onSave() {
+  if (this.name !== '' && this.phone !== '') {
+    this.$store.dispatch('createOrder', {
+      name: this.name,
+      phone: this.phone,
+      adId: this.ad.id,
+      userId: this.ad.userId
+    })
+    .finally(() => {
+      this.name = ''
+      this.phone = ''
+      this.modal = false
+    })
+  }
+}
     }
 }
 </script>
